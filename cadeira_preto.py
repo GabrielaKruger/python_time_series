9ae4b1c3c53daae5dadaa8a85219577575cbc5e6
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-27 14:59:03 -0300

    Update cadeira_preto.py
    exercícios

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,22 @@
+# formate o cabeçalho, depois de ter feito os exercícios abaixo
+
+
+def e_par(n: int) -> bool:
+    """Retorna True se n é par, senão False."""
+    # TODO: retorne se n é par
+    ...
+
+
+def limpa_texto(s: str) -> str:
+    """Deixa minúsculo e remove pontuação comum."""
+    # TODO: converta s para minúsculo e remova pontuações como ,.;:!?'"()-_
+    ...
+
+def total_por_vendedor(vendas):
+    """
+    vendas: lista de tuplas (nome, valor).
+    Retorna: dict {nome: soma_valores}
+    """
+    # TODO: inicialize um dict e vá somando
+    ...
 
